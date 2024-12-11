667838a053a04c8dc8e0328604f110cac479f0a7
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-12-11 06:48:18 -0300

    Add Home component and integrate routing: create Home component for user welcome page and add react-router-dom for navigation

---
+++ shipkeep-app/frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home Page!</h2>
+      <p>You are now logged in.</p>
+      {/* Add more content here */}
+    </div>
+  );
+}
+
+export default Home;
